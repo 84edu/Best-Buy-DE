7ee712a5b89a18f21d183e072254b91a37e69cc4
Behrooz Mohandeszadeh <behrooz@mohandeszadeh.com>
2026-02-12 15:28:44 +0100

    implement Store class and integrated ordering system

---
+++ store.py
@@ -0,0 +1,43 @@
+import products
+from typing import List
+
+
+class Store:
+
+    def __init__(self, products):
+        self.products = products
+
+
+    def add_product(self, product):
+        self.products.append(product)
+
+
+    def remove_product(self, product):
+        self.products.remove(product)
+
+
+    def get_total_quantity(self) -> int:
+        total_items = 0
+        for product in self.products:
+            total_items += product.get_quantity()
+
+        return total_items
+
+
+    def get_all_products(self) -> List[products.Product]:
+        active_items = []
+        for product in self.products:
+            if product.is_active():
+                active_items.append(product)
+
+        return active_items
+
+
+    def order(self, shopping_list) -> float:
+        total_price = 0
+        for product, quantity in shopping_list:
+            price = product.buy(quantity)
+            total_price += price
+
+        return total_price
+
