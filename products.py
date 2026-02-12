0b212478bb2b0db91eb45a3bf550c6814d7f2f43
Behrooz Mohandeszadeh <behrooz@mohandeszadeh.com>
2026-02-12 14:23:32 +0100

    implement Product class with inventory logic

---
+++ products.py
@@ -0,0 +1,59 @@
+class Product:
+
+    def __init__(self, name, price, quantity):
+        if not name:
+            raise ValueError("The name must be given!")
+
+        if price < 0:
+            raise ValueError("The price con not be a negative number!")
+
+        if quantity < 0:
+            raise ValueError("The Quantity can not be a negative number!")
+
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+        self.active = True
+
+
+    def get_quantity(self) -> int:
+        return self.quantity
+
+
+    def set_quantity(self, quantity):
+        self.quantity = quantity
+
+        if quantity == 0:
+            self.deactivate()
+
+
+    def is_active(self) -> bool:
+        return self.active
+
+
+    def activate(self):
+        self.active = True
+
+
+    def deactivate(self):
+        self.active = False
+
+
+    def show(self):
+        print(f"{self.name}, Price: {self.price}, Quantity: {self.quantity}")
+
+
+    def buy(self, quantity) -> float:
+        if self.quantity < quantity:
+            raise ValueError("Not enough in stock!")
+
+        if not self.is_active():
+            raise Exception("Product is not active!")
+
+        total_price = self.price * quantity
+
+        new_quantity = self.quantity - quantity
+        self.set_quantity(new_quantity)
+
+        return total_price
+
